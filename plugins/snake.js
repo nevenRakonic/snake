8907f503f5010ac3314ad50d2e9f745cec25e491
Neven <neven@nerk711.(none)>
2014-04-22 19:14:34 +0200

    further canvas fiddling

--- plugins/snake.js
+++ plugins/snake.js
@@ -1,10 +1,42 @@
 window.onload = function() {
   //Snake namespace
-  var Snake =  {};
+  var Snake = {};
+
+
+  Snake.posA = 0;
+  Snake.posB = 0;
 
   Snake.canvas = document.getElementById("snake");
+  Snake.main = document.getElementById("main");
   Snake.context = Snake.canvas.getContext("2d");
-  Snake.context.fillRect(50,25,150,100);
+  //Snake.context.fillRect(0,0,10,10);
+
+
+  for (var x = 0.5; x < 500; x += 10) {
+    Snake.context.moveTo(x, 0);
+    Snake.context.lineTo(x, 375);
+  }
+  for (var y = 0.5; y < 375; y += 10) {
+    Snake.context.moveTo(0, y);
+    Snake.context.lineTo(500, y);
+  }
+
+  Snake.context.strokeStyle = "#eee";
+  Snake.context.stroke();
+
+  /*
+  Snake.animate = function(){
+    Snake.posA += 10;
+    Snake.posB += 10;
+  }
+  function start() {
+    setInterval(Snake.animate, 1000);
+  }
+  */
+
+
 }
+
+
 
 
