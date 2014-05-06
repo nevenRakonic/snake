17863b7208e4eb0565ba386ea1ceebc3dc7fefbf
Neven <neven@nerk711.(none)>
2014-05-07 00:34:10 +0200

    basic movement

--- plugins/snake.js
+++ plugins/snake.js
@@ -16,18 +16,17 @@ window.onload = function() {
   var App = {};
   App.run = true;
   App.frame = 0;
+  //is used to bind keyevents to the direction of snake,
+  //n = north, e = east etc.
+  App.eventDict = {87: 'n', 68: 'e', 65: 'w', 83: 's'}
 
   /* Snake object is used to describe the snake */
   var Snake = {};
   Snake.part_length = 20;
   Snake.height = 20;
   Snake.parts = [];
-  Snake.bearing = "e";
-  Snake.initialize_parts = function(snake_size){
-    for (var i = 0; i < snake_size; i++){
-      Snake.parts.push([i*20, 200])
-    }
-  }
+  Snake.direction = 'e';
+
 
   var canvas = document.getElementById("snake");
   var c = canvas.getContext("2d");
@@ -41,22 +40,57 @@ window.onload = function() {
 
   }
 
-  Snake.recreate = function(){
-    var size = Snake.parts.length;
-    var tail = Snake.parts[0];
-    var head = Snake.parts[size - 1];
+  Snake.initializeParts = function(snake_size){
+    for (var i = 0; i < snake_size; i++){
+      Snake.parts.push([i*20, 200])
+    }
+  }
+
+  Snake.borderDetect = function(head, size){
 
     if (head[0] > canvas.width - Snake.part_length*size){
       Snake.parts.shift();
       Snake.parts.push([0, head[1]]);
     }
-    else {
-      Snake.parts.shift();
-      Snake.parts.push([head[0] + Snake.part_length, head[1]]);
-    }
+
   }
 
-  App.animate = function(){
+  Snake.move = function(head, size) {
+
+    Snake.parts.shift();
+    switch(Snake.direction){
+      case 'e':
+        Snake.parts.push([head[0] + Snake.part_length, head[1]]);
+        break;
+      case 'w':
+        Snake.parts.push([head[0] - Snake.part_length, head[1]]);
+        break;
+      case 'n':
+        Snake.parts.push([head[0], head[1] - Snake.part_length]);
+        break;
+      case 's':
+        Snake.parts.push([head[0], head[1] + Snake.part_length]);
+        break;
+
+    }
+
+
+
+  }
+
+  Snake.recreate = function(){
+    var size = Snake.parts.length;
+    var tail = Snake.parts[0];
+    var head = Snake.parts[size - 1];
+
+    //Snake.borderDetect(head, size);
+    Snake.move(head, size);
+
+  }
+
+
+
+  App.draw = function(){
     c.clearRect(0, 0,canvas.width, canvas.height);
     //foreach loop might be better
     for (var i = 0; i < Snake.parts.length; i++){
@@ -65,14 +99,9 @@ window.onload = function() {
   }
 
   App.keyEvent = function(event){
-    console.log(event.keyCode);
-    if (event.keyCode === 87){
-      Snake.bearing = "n";
-    }
-    else if (event.keyCode === 68){
-      Snake.bearing = "e";
-    }
-
+    var possible_direction = App.eventDict[event.keyCode];
+    if (possible_direction !== undefined)
+      Snake.direction = possible_direction; console.log(Snake.direction);
   }
 
   App.loop = function() {
@@ -81,8 +110,8 @@ window.onload = function() {
       window.requestAnimFrame(App.loop);
 
       //lowers animation speed
-      if (App.frame % 10 === 0) {
-        App.animate();
+      if (App.frame % 8 === 0) {
+        App.draw();
         //Snake.checkBoundaries();
         Snake.recreate();
       }
@@ -93,7 +122,7 @@ window.onload = function() {
 
   /* pre loop calls */
   // determines the initial size of the snake
-  Snake.initialize_parts(8)
+  Snake.initializeParts(8)
 
   /* main loop */
   App.loop();
