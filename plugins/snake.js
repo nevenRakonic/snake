c9b5474eb3e9faae33e443ddcc1a987441f52214
Neven <neven@nerk711.(none)>
2014-06-05 23:49:36 +0200

    refactored the movement a bit, bug still left

--- plugins/snake.js
+++ plugins/snake.js
@@ -31,7 +31,6 @@
     App.run = false;
     App.gameOver = false;
 
-
     //is used to bind keyevents to the direction of snake,
     //n = north, e = east etc.
     App.eventDict = {87: 'n', 68: 'e', 65: 'w', 83: 's', 32: 'PAUSE'};
@@ -40,6 +39,8 @@
     var Snake = {};
     Snake.part_length = 20;
     Snake.height = 20;
+    /* this dict is used to prevent player from reversing the snake*/
+    Snake.opposite_direction = {'n':'s', 's':'n','w':'e','e':'w'}
 
 
     /* food object */
@@ -54,28 +55,15 @@
       }
     };
 
-    App.initialize = function(){
 
-      /* stuff that isn't constant has to be reset */
-      App.frame = 0;
-      App.score = 0;
-      //each part of the snake is a member of parts array that shows X,Y position of that part
-      Snake.parts = [];
-      Snake.direction = 'e';
-      Snake.toGrow = [];
-      // determines the initial size of the snake
-      Snake.initializeParts(20);
-
-      Food.position = [];
-      Food.exist = false;
-    };
-
+    /* creates the snake at the beginning */
     Snake.initializeParts = function(snake_size){
       for (var i = 0; i < snake_size; i++){
         Snake.parts.push([i*20, 200]);
       }
     };
 
+    /* checks if snake has eaten the food, updates related information */
     Snake.checkIfMunch = function(head) {
       if(head[0] === Food.position[0] && head[1] === Food.position[1]){
         Food.exist = false;
@@ -143,6 +131,8 @@
       var tail = Snake.parts[0];
       var head = Snake.parts[size - 1];
 
+
+      //if snake ate a part, then it will grow at the tail so it won't be deleted
       if (tail[0] !== Snake.toGrow[0]){
         Snake.parts.shift();
       }
@@ -156,6 +146,22 @@
       //redrawn on the other side, if not it's a regular move
       if(!Snake.borderDetect(head))
         Snake.move(head);
+    };
+
+    App.initialize = function(){
+      /* stuff that isn't constant has to be reset so parts of objects are initialized here */
+      App.frame = 0;
+      App.score = 0;
+      //each part of the snake is a member of parts array that shows X,Y position of that part
+      Snake.parts = [];
+      Snake.direction = 'e';
+      // keeps to track of where the snake has eaten the food, it's where it will grow after the tail passes it
+      Snake.toGrow = [];
+      // determines the initial size of the snake
+      Snake.initializeParts(20);
+
+      Food.position = [];
+      Food.exist = false;
     };
 
     App.generateRandomXYPosition = function(){
@@ -179,7 +185,6 @@
       }
 
       //takes care of food
-
       c.fillRect(Food.position[0], Food.position[1], Food.length, Food.height);
       c.fillStyle = 'red';
       c.fillText('' + App.score, 500, 50);
@@ -204,8 +209,11 @@
           App.draw();
           Snake.recreate();
           Food.recreate();
+          App.in_logic = true;
         }
+
         App.frame += 1;
+        App.in_logic = false;
 
         if (App.gameOver){
           App.initialize();
@@ -217,6 +225,8 @@
     };
 
 
+
+
     /* main loop */
     App.startNew();
 
@@ -224,6 +234,7 @@
     App.keyEvent = function(event){
 
       var key = App.eventDict[event.keyCode];
+      /*checks if pause key should pause the running game, or restart it if it's over*/
       if (key === 'PAUSE'){
         App.changeRunning();
         if (!App.gameOver){
@@ -233,16 +244,16 @@
       }
 
       //this is nasty, nasty, nasty! :[
-      if (key === 'n' && Snake.direction === 's')
-        return null;
-      if (key === 's' && Snake.direction === 'n')
-        return null;
-      if (key === 'w' && Snake.direction === 'e')
-        return null;
-      if (key === 'e' && Snake.direction === 'w')
-        return null;
+      // if (key === 'n' && Snake.direction === 's')
+      //   return null;
+      // if (key === 's' && Snake.direction === 'n')
+      //   return null;
+      // if (key === 'w' && Snake.direction === 'e')
+      //   return null;
+      // if (key === 'e' && Snake.direction === 'w')
+      //   return null;
 
-      if (key !== undefined)
+      if (key !== undefined && Snake.opposite_direction[key] !== Snake.direction)
         Snake.direction = key; console.log(Snake.direction);
     };
     window.onclick = function (){ App.changeRunning(); App.startNew(); };
